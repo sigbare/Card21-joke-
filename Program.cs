5120cf57d9ae7f443e1f2349c1b1ad47f1b62961
ADMIN <qwertysdfg77@gmail.com>
2025-01-24 14:50:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace testSomething
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CardDeck cardDeck = new CardDeck();
+
+            cardDeck.CreateCardDeck();
+
+            List<Card> cards = new List<Card>(cardDeck.GetCardDeck());
+
+            Shuffle<Card>(cards);
+
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name + " " + card.CardType);
+            }
+        }
+
+
+
+        private static Random rng = new Random();
+        public static void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+        }
+    }
+}
---
+++ card.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testSomething
+{
+   
+    enum CardValue
+    {
+        
+    }
+
+
+
+    public class CardDeck
+    {
+        Dictionary<int, string> cardsName = new Dictionary<int, string>
+        {
+            {0, "six" },
+            {1, "seven" },
+            {2, "eight" },
+            {3, "nine" },
+            {4, "ten" },
+            {5, "jack" },
+            {6, "queen" },
+            {7, "king" },
+            {8, "ace" }
+           
+        };
+        Dictionary<int, string> cardsType = new Dictionary<int, string>
+        {
+            {0,"heart" },
+            {1,"dimond" },
+            {2,"club" },
+            {3,"spade" }
+        };
+
+        const int NumberCards = 36; 
+        List<Card> cards = new List<Card>();
+        
+        public void CreateCardDeck()
+        {
+            cards.Clear();
+            
+            for(int i = 0; i < 4; i++)
+            {
+
+                for(int  j = 0; j < 9; j++)
+                {
+                    cards.Add(new Card(j, cardsType[i], cardsName[j]));
+                }
+   
+            }
+
+        }
+
+        public List<Card> GetCardDeck()
+        {
+            return cards; 
+        }
+
+    }
+
+
+    public class Card
+    {
+        public int value { get; set; }
+        public string CardType { get; set; } 
+        public string Name { get; set; }
+
+
+
+        public Card(int value, string CardType, string Name) 
+        {
+            this.value = value;
+            this.CardType = CardType;
+            this.Name = Name;
+        }
+
+
+    }
+
+
+}
