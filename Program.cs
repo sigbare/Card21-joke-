cddf349694e0fd0c3246411f2e173bcc506682e9
vbif fdlei <qwertysdfg77@gmail.com>
2025-01-24 18:31:34 +0300

    creat info file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace testSomething
+﻿using System;
+using System.IO;
+
+namespace testSomething
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
             CardDeck cardDeck = new CardDeck();
 
             cardDeck.CreateCardDeck();
@@ -12,13 +16,75 @@
 
             Shuffle<Card>(cards);
 
-            foreach (Card card in cards)
-            {
-                Console.WriteLine(card.Name + " " + card.CardType);
-            }
+
+            var command = StartGame();
+            if (command == "info");
+            info();
+
+
+
+            Console.ReadLine();
         }
 
+        public static void info()
+        {
+            string filePath = Environment.CurrentDirectory + @"\ruls.txt";
+            if(!File.Exists(filePath))
+            {
+                Console.WriteLine("file info error");
+                return;
+            }
 
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("error Read file info");
+            }
+            
+
+        }
+
+        public static void NewGame()
+        {
+
+        }
+
+        public static string? StartGame() 
+        { 
+           Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hello it's CardsGame like BlackJack but with new rulse");
+            Console.WriteLine("if you wanna read rulse please write info ");
+            Console.WriteLine("if you wanna start game write start");
+
+            Console.WriteLine("________________________________");
+
+            string? str = Console.ReadLine();
+            bool g = false;
+            while (!g)
+            {
+                if (str != null && (str == "info" || str == "start"))
+                {
+                    return str;
+                    g = true;
+                }
+                str = Console.ReadLine();
+            }
+           
+                
+            Console.WriteLine("pls check command");
+
+            return null;
+        
+        }
 
         private static Random rng = new Random();
         public static void Shuffle<T>(IList<T> list)
